a1fa5b3fb171c2c564a0e1c56f65b7c87d9ff11c
filipmellgren <filip.mellgren@gmail.com>
2019-12-28 14:56:18 +0100

    first commit

---
+++ dqn_main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 28 14:47:03 2019
+
+@author: filip
+
+!git remote add origin https://github.com/filipmellgren/DQN_pricing_algorithms.git
+"""
+
